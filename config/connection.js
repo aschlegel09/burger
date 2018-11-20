4a6d44dabd38b35c3c4e561299542592708e793d
aschlegel <aschlegel09@gmail.com>
2018-11-19 20:24:17 -0500

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'jbvr1wa9977rq29e',
+        password: 'xfw5ovaa9p6vdjie',
+        database: 'wjtmmawnzhgj3f3w'
+    });
+};
+
 connection.connect(function(err) {
     if (err) {
         console.error("error with connecting: " + err.stack);
